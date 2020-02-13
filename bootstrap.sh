48d0f1d91d513fe777c63f322c9f51f3c3e1c49c
Christopher Baklid <Inveracity@users.noreply.github.com>
2020-02-13 13:02:44 +0100

    update to python 3.8 and remove fluff

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,20 +5,12 @@ apt install -y software-properties-common
 apt install -y python-software-properties
 apt install -y curl
 
-# Python 3.7
+# Python 3.8
 add-apt-repository -y ppa:deadsnakes/ppa
-
-# Rethinkdb
-curl -fsSL https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
-echo "deb http://download.rethinkdb.com/apt xenial main" | tee /etc/apt/sources.list.d/rethinkdb.list
 
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Saltstack
-curl -fsSL https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
-add-apt-repository "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main"
 
 # Golang
 add-apt-repository -y ppa:longsleep/golang-backports
@@ -32,71 +24,25 @@ apt update
 apt install -y unzip
 apt install -y apt-transport-https
 apt install -y docker-ce
-apt install -y rethinkdb
-apt install -y python3.7
-apt install -y python3.7-dev
+apt install -y python3.8
+apt install -y python3.8-distutils
+apt install -y python3.8-dev
 apt install -y build-essential
-apt install -y salt-master salt-minion
 apt install -y golang-go
 apt install -y google-cloud-sdk
 apt install -y kubectl
 
 # Compose
-curl -Ls https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -Ls https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-# Terraform
-curl -Ls https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip -o terraform.zip
-unzip terraform.zip
-mv terraform /usr/bin/terraform
-rm -rf terraform.zip
-
-# Nomad
-curl -Ls https://releases.hashicorp.com/nomad/0.8.3/nomad_0.8.3_linux_amd64.zip -o nomad.zip
-unzip -q nomad.zip
-mv nomad /usr/local/bin/nomad
-chmod +x /usr/local/bin/nomad
-rm -rf nomad.zip
 
 # Pip
 curl -s https://bootstrap.pypa.io/get-pip.py | python -
 curl -s https://bootstrap.pypa.io/get-pip.py | python3 -
-curl -s https://bootstrap.pypa.io/get-pip.py | python3.7 -
+curl -s https://bootstrap.pypa.io/get-pip.py | python3.8 -
 
 # Pipenv
-python3.7 -m pip install pipenv
-
-# Saltstack config
-service salt-master stop
-service salt-minion stop
-
-tee /etc/salt/master.d/default.conf << END
-file_roots:
-  base:
-    - /vagrant/salt
-log_level: debug
-auto_accept: True
-END
-
-service salt-master start
-
-tee /etc/salt/minion.d/default.conf << END
-master: 127.0.0.1
-log_level: debug
-END
-
-service salt-minion start
-
-# RethinkDB config
-tee /etc/rethinkdb/instances.d/rethinkdb.conf <<EOF
-runuser=root
-rungroup=root
-bind=0.0.0.0
-driver-port=28015
-http-port=28010
-EOF
-
-service rethinkdb restart
+python3.8 -m pip install pipenv
 
 # Environment variables
 cat > /etc/environment <<EOL
